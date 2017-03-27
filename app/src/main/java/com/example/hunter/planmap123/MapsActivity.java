29698cc52e83050fe444321fe2151f6b8a3f0f21
rahul-soshte <rahul.soshte47@gmail.com>
2017-03-28 03:24:44 +0530

    Many

--- app/src/main/java/com/example/hunter/planmap123/MapsActivity.java
+++ app/src/main/java/com/example/hunter/planmap123/MapsActivity.java
@@ -1,7 +1,10 @@
 package com.example.hunter.planmap123;
 
+import android.graphics.Color;
+import android.graphics.Point;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
@@ -11,20 +14,24 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+public class MapsActivity extends FragmentActivity implements GoogleMap.OnMapLongClickListener,OnMapReadyCallback {
 
     private ArrayList<Places> LatSort=new ArrayList<Places>();
   private ArrayList<Places> LongSort=new ArrayList<Places>();
+private List <Draggable> mCircles=new ArrayList<Draggable>();
 
    private LatLngBounds bound;
 private LatLng Southwest;
@@ -34,6 +41,29 @@ private LatLng Southwest;
     double MaxLong;
     double MinLong;
 
+public class Draggable
+
+{
+    private final Circle circle;
+    private Marker centerMarker;
+    private double radius;
+
+    public Draggable(LatLng center, LatLng radiusLatLng) {
+        this.radius = toRadiusMeters(center, radiusLatLng);
+
+        circle = mMap.addCircle(new CircleOptions().center(center).radius(radius).strokeColor(Color .BLACK).fillColor(5));
+
+
+    }
+
+}
+    private static double toRadiusMeters(LatLng center,LatLng radius)
+    {
+        float[] result =new float[1];
+        Location.distanceBetween(center.latitude,center.longitude,radius.latitude,radius.longitude,result);
+        return result[0];
+
+    }
 
     private GoogleMap mMap;
     private ArrayList<Places> drenched=new ArrayList<Places>();
@@ -52,7 +82,8 @@ Iterator itr,itr1;
     public void onMapSearch(View view) {
         EditText locationSearch = (EditText) findViewById(R.id.editText);
         String location = locationSearch.getText().toString();
-        List<Address> addressList = null;
+
+        List <Address> addressList = null;
         if ( location!= null || !location.equals("")) {
             Geocoder geocoder=new Geocoder(this);
             try{
@@ -60,8 +91,6 @@ Iterator itr,itr1;
 
             }catch(IOException e){
                 e.printStackTrace();
-
-
             }
             Address address=addressList.get(0);
             LatLng latLng=new LatLng(address.getLatitude(),address.getLongitude());
@@ -69,20 +98,28 @@ Iterator itr,itr1;
             mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
             p1=new Places(location,address.getLongitude(), address.getLatitude());
             drenched.add(p1);
-
         }
 
 
     }
     public void onFindCenter(View v)
     {
-        Collections.sort(drenched,Places.StuLatitudeComparator);
-        LatSort.addAll(drenched);
-        Collections.sort(drenched,Places.StuLongitudeComparator);
-        LongSort.addAll(drenched);
+        if(drenched.size()==1)
+        {
+            //Do nothing
+        }
+        else if(drenched.size()!=1)
+        {
+
+        }
 //Conditions for Southwest
         //MinLat and MinLong
-        
+    }
+    public void ClearMarkers(View v)
+    {
+        mMap.clear();
+        mCircles.clear();
+
     }
 
     /**
@@ -98,6 +135,19 @@ Iterator itr,itr1;
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+        mMap.setOnMapLongClickListener(this);
+    }
+
+    @Override
+    public void onMapLongClick(LatLng point)
+    {
+        //We know the center let's place the outline at a point 3/4
+        View view=((SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map)).getView();
+        LatLng radiusLatLng=mMap.getProjection().fromScreenLocation(new Point(view.getHeight()*3/4,view.getWidth()*3/4));
+        Draggable circle1 =new Draggable(point,radiusLatLng);
+mCircles.add(circle1);
+
     }
 
 }
